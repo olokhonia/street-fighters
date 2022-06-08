49243ccf51e31e70389043e3a978ef8451379c45
Oleksandr Lokhonia <olokhonia@opengov.com>
2022-06-08 14:59:04 +0300

    updated winner.js and implemented showWinnerModal method

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,19 @@
+import {showModal} from "./modal";
+import {createElement} from "../../helpers/domHelper";
+import {createFighterImage} from "../fighterPreview";
+import App from "../../app";
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+    // call showModal function
+    const title = `${fighter.name} is WINNER 🏆🏆🏆`;
+    const winnerDetails = createElement({tagName: 'div'});
+    const winnerImageElement = createFighterImage(fighter);
+
+    winnerDetails.append(winnerImageElement);
+
+    showModal({
+        title, bodyElement: winnerDetails, onClose: () => {
+            App.restartApp();
+        }
+    });
 }
