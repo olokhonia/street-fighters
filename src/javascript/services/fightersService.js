8dbd7942e3fc09fa692390b402d3fcfe2ba36c66
Oleksandr Lokhonia <olokhonia@opengov.com>
2022-06-08 15:02:35 +0300

    implement getFighterDetails to get fighter details

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -15,6 +15,13 @@ class FighterService {
   async getFighterDetails(id) {
     // todo: implement this method
     // endpoint - `details/fighter/${id}.json`;
+    const endpoint = `details/fighter/${id}.json`;
+    try {
+      const apiResult = await callApi(endpoint);
+      return apiResult;
+    } catch(error) {
+      throw error;
+    }
   }
 }
 
